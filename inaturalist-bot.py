edab2908f1871f06f4c784f985b03da6004be9a2
B-Lei <benjamin.g.lei@gmail.com>
2024-04-13 15:41:55 -0700

    Improve code organization

--- inaturalist-bot.py
+++ inaturalist-bot.py
@@ -5,6 +5,9 @@ from discord.ext import commands
 from pyinaturalist import get_observations
 from datetime import timedelta, datetime
 
+# -------------------------------------
+# Discord Bot Initialization
+# -------------------------------------
 help_command = commands.DefaultHelpCommand(
     no_category = 'Commands',
     show_parameter_descriptions=False,
@@ -18,10 +21,13 @@ bot = commands.Bot(
     help_command=help_command
 )
 
+# -------------------------------------
+# Global Variables
+# -------------------------------------
 observation_cache = set()   # Cache to avoid returning the same observation
-taxon_cache = set()         # Cache to avoid returning the same taxa
+taxon_cache = set()         # Cache to avoid returning the same taxon
 hide_names = False
-taxon_whitelist = {  # Avoid blacklisting major taxa
+supported_taxa = {
     'Aves': 3,
     'Fungi': 47170,
     'Amphibia': 20978,
@@ -33,28 +39,16 @@ taxon_whitelist = {  # Avoid blacklisting major taxa
     'Actinopterygii': 47178,
 }
 
-@bot.event
-async def on_ready():
-    print('Logged in as {0.user}'.format(bot))
-
-@bot.event
-async def on_command_error(ctx, error):
-    if isinstance(error, discord.ext.commands.errors.CommandNotFound):
-        await ctx.send("Unknown command.\nPlease run '&help' for a list of commands.")
-
-def clear_caches():  # Every 24 hours, clear all caches
-    global observation_cache, taxon_cache
-    observation_cache = set()
-    taxon_cache = set()
-    threading.Timer(86400, clear_caches).start()  
-
-def get_observation(taxa, query=None):
+# -------------------------------------
+# Function Definitions
+# -------------------------------------
+def get_observation(taxon, query=None):
     not_ids = list(observation_cache)
     not_taxons = list(taxon_cache)
-    whitelist = list(taxon_whitelist.values())
+    whitelist = list(supported_taxa.values())  # Avoid blacklisting major taxa
     observations = get_observations(
         place_id='any',
-        iconic_taxa=taxa,
+        iconic_taxa=taxon,
         photos=True,
         identified=True,
         page=1,
@@ -78,6 +72,56 @@ def get_observation(taxa, query=None):
     }
     return details
 
+async def send_observation_message(taxon, ctx, query):
+    try:
+        observation = get_observation(taxon, query)
+        if not observation:
+            await ctx.send('No results found.')
+            return
+        if hide_names:
+            title = f"Common Name: ||{observation['preferred_common_name']}||" if observation['preferred_common_name'] != 'N/A' else f"Scientific Name: ||{observation['scientific_name']}||"
+        else:
+            title = f"Common Name: {observation['preferred_common_name']}" if observation['preferred_common_name'] != 'N/A' else f"Scientific Name: {observation['scientific_name']}"
+        embed = discord.Embed(title=title)
+        embed.set_image(url=observation['photo_url'])
+        fields = {
+            'Common Name': observation['preferred_common_name'] if observation['preferred_common_name'] not in title else '',
+            'Scientific Name': observation['scientific_name'] if observation['scientific_name'] not in title else '',
+            'Wikipedia Link': observation['wikipedia_url'],
+            'iNaturalist Link': observation['uri'],
+        }
+        for name, value in fields.items():
+            if value:
+                if hide_names and name != 'iNaturalist Link':
+                    embed.add_field(name=name, value=f"||{value}||")  # Add spoiler tags
+                else:
+                    embed.add_field(name=name, value=value)
+        await ctx.send(embed=embed)
+    except Exception as e:
+        print('Error occurred:', e)
+        await ctx.send("Sorry, I had trouble retrieving an image.")
+
+def clear_caches():  # Every 24 hours, clear all caches
+    global observation_cache, taxon_cache
+    observation_cache = set()
+    taxon_cache = set()
+    threading.Timer(86400, clear_caches).start()  
+
+# -------------------------------------
+# Discord Bot Event Handling
+# -------------------------------------
+@bot.event
+async def on_ready():
+    print('Logged in as {0.user}'.format(bot))
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, discord.ext.commands.errors.CommandNotFound):
+        await ctx.send("Unknown command.\nPlease run '&help' for a list of commands.")
+
+# -------------------------------------
+# Discord Bot Command Definitions
+# -------------------------------------
 @bot.command(brief="Bird pics", description="")
 async def birdpic(ctx, *args):
     await send_observation_message("Aves", ctx, " ".join(args))
@@ -131,34 +175,12 @@ async def nameshow(ctx):
     hide_names = False
     await ctx.send('Names will now be displayed without spoiler tags.')
 
-async def send_observation_message(taxa, ctx, query):
-    try:
-        observation = get_observation(taxa, query)
-        if not observation:
-            await ctx.send('No results found.')
-            return
-        if hide_names:
-            title = f"Common Name: ||{observation['preferred_common_name']}||" if observation['preferred_common_name'] != 'N/A' else f"Scientific Name: ||{observation['scientific_name']}||"
-        else:
-            title = f"Common Name: {observation['preferred_common_name']}" if observation['preferred_common_name'] != 'N/A' else f"Scientific Name: {observation['scientific_name']}"
-        embed = discord.Embed(title=title)
-        embed.set_image(url=observation['photo_url'])
-        fields = {
-            'Common Name': observation['preferred_common_name'] if observation['preferred_common_name'] not in title else '',
-            'Scientific Name': observation['scientific_name'] if observation['scientific_name'] not in title else '',
-            'Wikipedia Link': observation['wikipedia_url'],
-            'iNaturalist Link': observation['uri'],
-        }
-        for name, value in fields.items():
-            if value:
-                if hide_names and name != 'iNaturalist Link':
-                    embed.add_field(name=name, value=f"||{value}||")  # Add spoiler tags
-                else:
-                    embed.add_field(name=name, value=value)
-        await ctx.send(embed=embed)
-    except Exception as e:
-        print('Error occurred:', e)
-        await ctx.send("Sorry, I had trouble retrieving an image.")
+# -------------------------------------
+# Main Execution
+# -------------------------------------
+def main():
+    clear_caches()  # Start the cache clearing timer
+    bot.run(os.environ["DISCORD_TOKEN"])
 
-clear_caches()  # Start the cache clearing timer
-bot.run(os.environ["DISCORD_TOKEN"])
+if __name__ == "__main__":
+    main()
